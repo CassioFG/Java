3f7641c28e4e4464d273038ff30c73fe6f7c2146
Cassio Fernando <cassiofernandogsg@gmail.com>
2024-06-20 17:32:53 -0300

    tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,5 +2,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
         
+        final double NUMERO_DE_PI = 3.14; // usasse final e caixa alta para remeter a uma variavel constante. 
+        int num = 10; // numeros inteiros. 
+        String nome = "Nando"; // textos 
+        boolean verdade = true; // tipos logicos (verdadeiro ou falso)
+        char letraA = 'a';
+        float num1 = 3.22f; // usasse float para numeros com casa decimal obs: ao usar float toda atribuição deverá ser finalizada com "F". 
+        long cpf = 495920023410l; // usasse long para suportar mais dados dentro da variável obs: ao usar long o valor atribuido deverá finalizar com "L". 
+
     }
 }
